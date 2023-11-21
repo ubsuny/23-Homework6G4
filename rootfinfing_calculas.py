ad578b74644556bca4214820f9d2250a9e6f6b30
sharmistharanit <143737948+sharmistharanit@users.noreply.github.com>
2023-11-21 00:47:09 -0500

    Update rootfinfing_calculas.py

--- rootfinfing_calculas.py
+++ rootfinfing_calculas.py
@@ -4,3 +4,18 @@ import cmath
 import numpy as np
 
 
+def root_print_header(algorithm, accuracy):
+    """Prints the header for the root-finding algorithm.
+
+    Parameters:
+    - algorithm (str): The name of the root-finding algorithm.
+    - accuracy (float): The specified accuracy for the root.
+    """
+    sys.stdout.write("\n ROOT FINDING using " + algorithm +
+                     "\n Requested accuracy = " +repr(accuracy) +
+                     "\n Step     Guess For Root          Step Size      " +
+                     "     Function Value" +
+                     "\n ----  --------------------  --------------------" +
+                     "  --------------------" + "\n")
+
+
