241c0bd659fd15ff47d83ea89c9d06d4ad3e82f0
sharmistharanit <143737948+sharmistharanit@users.noreply.github.com>
2023-11-21 00:49:46 -0500

    Update rootfinfing_calculas.py

--- rootfinfing_calculas.py
+++ rootfinfing_calculas.py
@@ -19,3 +19,18 @@ def root_print_header(algorithm, accuracy):
                      "  --------------------" + "\n")
 
 
+def root_print_step(step, x, dx, f_of_x):
+    """Prints the details of each iteration step in the root-finding algorithm.
+
+    Parameters:
+    - step (int): The current iteration step.
+    - x (float): The guess for the root.
+    - dx (float): The step size.
+    - f_of_x (float): The function value at the current guess.
+    """
+    sys.stdout.write(repr(step).rjust(5))
+    for val in [x, dx, f_of_x]:
+        sys.stdout.write("  " + repr(val).ljust(20))
+    sys.stdout.write("\n")
+
+
