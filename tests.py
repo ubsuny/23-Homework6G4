090e94eded053cb8e8779e99a0df968a21298f7f
Mosaddeq Hossain <66480280+Mosaddeq107@users.noreply.github.com>
2023-11-22 11:49:44 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,59 @@
+""" 
+This is a unit testing file for the integral and root finding
 """
-This
-is
-unit testing
-"""
+import numpy as np
+import pytest
+from calculus import simpson, trapezoid, adaptive_trapezoid
+
+def test_simpson():
+    # Define a sample function for testing
+    def f(x):
+        return x**2
+
+    # Initial guess for integration bounds and number of subintervals
+    a = 0
+    b = 1
+    n = 100
+
+    # Calculate the expected result
+    expected_result = 1/3
+
+    # Perform the integration using simpson and assert the result
+    result = simpson(f, a, b, n)
+    assert np.isclose(result, expected_result, rtol=1e-5)
+
+
+def test_trapezoid():
+    # Define a sample function for testing
+    def f(x):
+        return x**2
+
+    # Initial guess for integration bounds and number of subintervals
+    a = 0
+    b = 1
+    n = 100
+
+    # Calculate the expected result
+    expected_result = 1/3
+
+    # Perform the integration using trapezoid and assert the result
+    result = trapezoid(f, a, b, n)
+    assert np.isclose(result, expected_result, rtol=1e-5)
+
+
+def test_adaptive_trapezoid():
+    # Define a sample function for testing
+    def f(x):
+        return x**2
+
+    # Initial guess for integration bounds and accuracy
+    a = 0
+    b = 1
+    acc = 1e-5
+
+    # Calculate the expected result
+    expected_result = 1/3
+
+    # Perform the integration using adaptive_trapezoid and assert the result
+    result = adaptive_trapezoid(f, a, b, acc)
+    assert np.isclose(result, expected_result, rtol=1e-5)
